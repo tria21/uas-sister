be9656e58fbfd8673ec0df34b8d812af8a7ab839
tria21 <sitiamaliafitriani@gmail.com>
2021-12-24 22:12:03 +0700

    bismillah bisa

--- php/src/config.php
+++ php/src/config.php
@@ -1,22 +1,12 @@
 <?php
-/**
- * using mysqli_connect for database connection
- */
-$server = "db";
-$user = "root";
-$pass = "";
-$database = "crud_db";
- 
-$mysqli = mysqli_connect($server, $user, $pass, $database);
 
-// $databaseHost = 'localhost';
-// $databaseUsername = 'root';
-// $databasePassword = '';
-// $databaseName = 'crud_db';
- 
-// $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'crud_db';
 
-if (!$mysqli) {
-    die("<script>alert('Gagal tersambung dengan database.')</script>");
+$mysqli = new mysqli($server, $user, $password, $db);
+if ($mysqli->connect_error) {
+    die('Koneksi Database gagal :' . $mysqli->connect_error);
 }
 ?>
