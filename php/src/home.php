f8ebb96d6c0259a84498f44f3e937429b944fabb
tria21 <sitiamaliafitriani@gmail.com>
2021-12-24 20:59:42 +0700

    bismillahhhhh

---
+++ php/src/home.php
@@ -0,0 +1,5 @@
+<div class="halaman">
+	<h2>Selamat datang di website saya</h2>
+    <p>Nama : Siti Amalia Fitriani</p>
+    <p>Politeknik Negeri Malang</p>
+</div>
--- php/src/index.php
+++ php/src/index.php
@@ -1,33 +1,50 @@
-<?php
-// Create database connection using config file
-include_once("config.php");
- 
-// Fetch all users data from database
-$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
-?>
- 
+<!DOCTYPE html>
 <html>
-<head>    
-    <title>Homepage</title>
+<head>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
 </head>
- 
 <body>
-<a href="add.php">Add New User</a><br/><br/>
+<div class="content">
+	<header>
+		<h1 class="judul">WEBSITE</h1>
+		<h3 class="deskripsi">Membuat Web Sederhana</h3>
+	</header>
  
-    <table width='80%' border=1>
+	<div class="menu">
+		<ul>
+			<li><a href="index.php?page=home">HOME</a></li>
+			<li><a href="index.php?page=user">USER</a></li>
+		</ul>
+	</div>
  
-    <tr>
-        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
-    </tr>
-    <?php  
-    while($user_data = mysqli_fetch_array($result)) {         
-        echo "<tr>";
-        echo "<td>".$user_data['name']."</td>";
-        echo "<td>".$user_data['mobile']."</td>";
-        echo "<td>".$user_data['email']."</td>";    
-        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
-    }
-    ?>
-    </table>
+	<div class="badan">
+ 
+ 
+	<?php 
+	if(isset($_GET['page'])){
+		$page = $_GET['page'];
+ 
+		switch ($page) {
+			case 'home':
+				include "home.php";
+				break;
+			case 'user':
+				include "users.php";
+				break;			
+			default:
+				echo "<center><h3>Maaf. Halaman tidak di temukan !</h3></center>";
+				break;
+		}
+	}else{
+		include "home.php";
+	}
+ 
+	 ?>
+ 
+	</div>
+</div>
 </body>
 </html>
